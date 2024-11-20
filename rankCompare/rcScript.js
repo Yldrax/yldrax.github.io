d0e3c847b7ef1c860aac108b753ce96222a0917f
Erik Schulz <eschulz2100@gmail.com>
2024-11-20 13:46:02 +0100

    Calculation Groundwork

--- rankCompare/rcScript.js
+++ rankCompare/rcScript.js
@@ -1,16 +1,11 @@
-const ranks = ["Iron 1","Iron 2","Iron 3","Bronze 1","Bronze 2","Bronze 3","Silver 1","Silver 2","Silver 3","Gold 1","Gold 2","Gold 3","Platinum 1","Platinum 2","Platinum 3","Diamond 1","Diamond 2","Diamond 3","Ascendant 1","Ascendant 2","Ascendant 3","Immortal 1","Immortal 2","Immortal 3","Radiant"]
-let r1 = 0;
-let r2 = 0;
-let r3 = 0;
-let r4 = 0;
-let r5 = 0;
-let r6 = 0;
-let r7 = 0;
-let r8 = 0;
-let r9 = 0;
-let r10 = 0;
+const ranks = ["Iron 1","Iron 2","Iron 3","Bronze 1","Bronze 2","Bronze 3","Silver 1","Silver 2","Silver 3","Gold 1","Gold 2","Gold 3","Platinum 1","Platinum 2","Platinum 3","Diamond 1","Diamond 2","Diamond 3","Ascendant 1","Ascendant 2","Ascendant 3","Immortal 1","Immortal 2","Immortal 3","Radiant"];
+let values = [0,0,0,0,0,0,0,0,0,0];
 
 
-function rValue(rank){
-    return ranks.indexOf(rank) + 1
+function avgRank(array){
+    let sum = 0;
+    for (let i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return Math.round(sum / array.length)
 }
